041c92ce9861a109b760456b047fef9567fd777f
Wilermo <william.dmo.sec@gmail.com>
2024-04-24 19:15:12 -0500

    Added CRUD for Persona

--- Controllers/PersonaController.cs
+++ Controllers/PersonaController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using personapi_dotnet.Models.Entities;
+using personapi_dotnet.Models.NewFolder;
+using personapi_dotnet.Models.ViewModels;
 
 namespace personapi_dotnet.Controllers
 {
@@ -13,14 +17,21 @@ namespace personapi_dotnet.Controllers
             _personaDbContext = personaDbContext;
         }
         // GET: PersonaController
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            var personas = _personaDbContext.Personas;
+            return View(await personas.ToListAsync());
         }
 
         // GET: PersonaController/Details/5
         public ActionResult Details(int id)
         {
+            var persona = _personaDbContext.Personas.Find(id);
+
+            if (persona != null)
+            {
+                return View(persona);
+            }
             return View();
         }
 
@@ -33,11 +44,36 @@ namespace personapi_dotnet.Controllers
         // POST: PersonaController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<ActionResult> Create(PersonaViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var persona = new Persona()
+                {
+                    Cc = model.Cc,
+                    Nombre = model.Nombre,
+                    Apellido = model.Apellido,
+                    Genero = model.Genero,
+                    Edad = model.Edad
+                };
+                _personaDbContext.Personas.Add(persona);
+                await _personaDbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        // GET: PersonaController/Edit/5
+        public async Task<ActionResult> Edit(int id)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                var persona_edit = _personaDbContext.Personas.Find(id);
+                if (persona_edit != null)
+                {
+                    return View(persona_edit);
+                }
+                return View();
             }
             catch
             {
@@ -45,20 +81,23 @@ namespace personapi_dotnet.Controllers
             }
         }
 
-        // GET: PersonaController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
         // POST: PersonaController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<ActionResult> Edit(Persona model)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                var persona_edit = _personaDbContext.Personas.Find(model.Cc);
+                if (persona_edit != null)
+                {
+                    _personaDbContext.Personas.Remove(persona_edit);
+                    _personaDbContext.Personas.Add(model);
+                    //_personaDbContext.Add(persona_edit);
+                    await _personaDbContext.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                return View();
             }
             catch
             {
@@ -69,22 +108,30 @@ namespace personapi_dotnet.Controllers
         // GET: PersonaController/Delete/5
         public ActionResult Delete(int id)
         {
+            var persona_delete = _personaDbContext.Personas.Find(id);
+            if (persona_delete != null)
+            {
+                return View(persona_delete);
+            }
             return View();
         }
 
         // POST: PersonaController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<ActionResult> Delete(Persona model)
         {
             try
             {
+                _personaDbContext.Personas.Remove(model);
+                await _personaDbContext.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
                 return View();
             }
+
         }
     }
 }
--- Controllers/TelefonoController.cs
+++ Controllers/TelefonoController.cs
@@ -138,3 +138,4 @@ namespace personapi_dotnet.Controllers
         }
     }
 }
+    
